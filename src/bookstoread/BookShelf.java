eaa02adec13a0a15789d10e4d0590d5f040f53be
Yao Marie Joelle <yaomariejoelle209@gmail.com>
2026-06-04 06:20:30 +0000

    methode shelfEmptyWhenNoBookAdded ajoutée

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,8 +1,15 @@
 package bookstoread;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 public class BookShelf {
+    private final List<String> books = new ArrayList<>();
+
     public List<String> books() {
-        return Collections.emptyList();
+        return Collections.unmodifiableList(books);
     }
+
 }
