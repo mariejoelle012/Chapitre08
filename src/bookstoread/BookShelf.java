70e7c630cf4841e452b5ad35c5f1e8b2cd89acd6
Yao Marie Joelle <yaomariejoelle209@gmail.com>
2026-06-04 09:52:41 +0000

    methode arrange() implémentée

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,9 +1,6 @@
 package bookstoread;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BookShelf {
     private final List<String> books = new ArrayList<>();
@@ -14,5 +11,10 @@ public class BookShelf {
 
     public void add(String...bookToAdd) {
         books.addAll(Arrays.asList(bookToAdd));
+    }
+
+    public List<String> arrange() {
+        books.sort(Comparator.naturalOrder());
+        return books;
     }
 }
--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,7 +30,12 @@ public class BookShelfSpec {
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
     }
 
-
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
     @Test
     void booksReturnedFromBookShelfIsImmutableForClient() {
         shelf.add("Effective Java", "Code Complete");
@@ -40,6 +46,12 @@ public class BookShelfSpec {
         } catch (Exception e) {
             assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
         }
+    }
+    @Test
+    void bookshelfArrangedByBookTitle() {
+        shelf.add("Effective Java", "Code Complete","The Mythical Man-Month" );
+        List<String> books = shelf.arrange();
+        assertEquals(Arrays.asList("Code Complete", "Effective Java", "The Mythical Man-Month"), books, () -> "Books in a bookshelf should be arranged lexicographically by book title");
     }
 
 }
