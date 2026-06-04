026bc3ff255dcb1c6a32012da503054356a4c197
Yao Marie Joelle <yaomariejoelle209@gmail.com>
2026-06-04 11:03:35 +0000

    objets ajoutés

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -4,17 +4,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
-    public List<String> books() {
+    public List<Book> books() {
         return Collections.unmodifiableList(books);
     }
 
-    public void add(String...bookToAdd) {
+    public void add(Book...bookToAdd) {
         books.addAll(Arrays.asList(bookToAdd));
     }
 
-    public List<String> arrange() {
+    public List<Book> arrange() {
         return books.stream().sorted().collect(Collectors.toList());}
 
 }
